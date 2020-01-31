4a73218f09adf09f639ba43265b116c91b28c49a
Gavin <karlgavinvalmadrid@gmail.com>
2020-01-31 12:35:18 -0500

    Space game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float horiz = Input.GetAxis("Horizontal");
+		GetComponent<RigidBody2d>().velocity = new Vector2(horiz, 0.0f);
+		// horiz = x
+		// 0 = y axis
+		// 0.0f = z axis
+    }
+}
