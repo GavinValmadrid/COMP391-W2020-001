aad46be00dc93cabf2d0b100d45663962c459c78
Gavin <karlgavinvalmadrid@gmail.com>
2020-01-31 13:43:06 -0500

    added player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    // Variables
+    public float speed;
+    public float minX, maxX, minY, maxY;
+
+    private Rigidbody2D rBody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 		float horiz = Input.GetAxis("Horizontal");
-		GetComponent<RigidBody2d>().velocity = new Vector2(horiz, 0.0f);
-		// horiz = x
-		// 0 = y axis
-		// 0.0f = z axis
+        float vert = Input.GetAxis("Vertical");
+
+        
+		rBody.velocity = new Vector2(horiz, vert) * speed;
+
+        rBody.position = new Vector2(
+             Mathf.Clamp(rBody.position.x, minX, maxX),
+             Mathf.Clamp(rBody.position.y, minY, maxY));
+
+        // horiz = x
+        // 0 = y axis
+        // 0.0f = z axis
     }
 }
