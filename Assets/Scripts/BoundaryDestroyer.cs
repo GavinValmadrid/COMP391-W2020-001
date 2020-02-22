ed15ab34f8ae0e275c7aeed3abb59d2d455f7c7f
Gavin <karlgavinvalmadrid@gmail.com>
2020-02-22 13:58:15 -0500

    added asteroid

---
+++ Assets/Scripts/BoundaryDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
